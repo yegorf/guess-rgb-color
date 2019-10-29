e9f1de74e61168c67cb1168e6a7316d73caa65ea
Yehor Khohlov <y.khohlov@pnn.in.ua>
2019-10-29 14:53:21 +0200

    add color picker

--- app/src/main/java/com/example/guess_rgb_color/fragment/RgbFragment.java
+++ app/src/main/java/com/example/guess_rgb_color/fragment/RgbFragment.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.guess_rgb_color.R;
+import com.skydoves.colorpickerpreference.ColorPickerView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RgbFragment extends Fragment {
+
+    @BindView(R.id.v_color_picker)
+    ColorPickerView colorPickerView;
+
+    @BindView(R.id.tv_rgb_code)
+    TextView colorTv;
+
+    @BindView(R.id.v_color_rgb)
+    View colorView;
 
     public static RgbFragment getInstance() {
         return new RgbFragment();
@@ -24,6 +36,14 @@ public class RgbFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rgb, container, false);
         ButterKnife.bind(this, view);
+
+        colorPickerView.setColorListener(colorEnvelope -> {
+            int[] colorArray = colorEnvelope.getColorRGB();
+            String color = String.format("%d, %d, %d", colorArray[0], colorArray[1], colorArray[2]);
+            colorTv.setText(color);
+            colorView.setBackgroundColor(android.graphics.Color.rgb(colorArray[0], colorArray[1], colorArray[2]));
+        });
+
         return view;
     }
 }
