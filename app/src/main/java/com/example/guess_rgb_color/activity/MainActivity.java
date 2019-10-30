625be32144ed166eec810c3e2fd9f636ce4cff3f
Yehor Khohlov <y.khohlov@pnn.in.ua>
2019-10-30 11:05:51 +0200

    add options menu

--- app/src/main/java/com/example/guess_rgb_color/activity/MainActivity.java
+++ app/src/main/java/com/example/guess_rgb_color/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.guess_rgb_color.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -23,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
                         .addToBackStack(null)
                         .add(R.id.fragment_container, GameFragment.getInstance());
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,0,0, getString(R.string.exit_from_application))
+                .setOnMenuItemClickListener(e -> {
+                    System.exit(0);
+                    return true;
+                });
+        menu.add(0,0,1, getString(R.string.share))
+                .setOnMenuItemClickListener(e -> {
+                    Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                    sharingIntent.setType("text/plain");
+                    String shareBody = "Download this cool game!";
+                    String shareSub = "Hello";
+                    sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                    sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                    startActivity(Intent.createChooser(sharingIntent, this.getString(R.string.share_using)));
+                    return true;
+                });
+        return true;
     }
 }
